dde608007c861bff9ba8c5e35941577166939c03
Yroshiii <104611761+Yroshiii@users.noreply.github.com>
2022-05-03 10:56:28 -0300

    Add files via upload

---
+++ aula026.py
@@ -0,0 +1,21 @@
+'''
+for / while
+0 10
+1 9
+2 8
+3 7
+4 6
+5 5
+6 4
+7 3
+8 2
+'''
+'''i = 0
+i2 = 10
+while i <= 10 and i2 >= 0:
+    print(i, i2)
+    i += 1
+    i2 -= 1'''
+for p, r in enumerate(range(10, 1, -1)):
+    print(p , r)
+
